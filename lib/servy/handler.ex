081d5ce561b890ba61aa99ce63dc3c0e8b63388e
Ashok Gelal <ashok@metageek.net>
2018-12-02 22:05:31 -0700

    Support post request type using recursion

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -26,6 +26,11 @@ defmodule Servy.Handler do
     |> File.read
     |> handle_file(conv)
   end
+
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!"}
+  end
+
   def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
@@ -135,4 +140,18 @@ Accept: */*
 
 response = Servy.Handler.handle(request)
 
+IO.puts response
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
+"""
+
+response = Servy.Handler.handle(request)
 IO.puts response

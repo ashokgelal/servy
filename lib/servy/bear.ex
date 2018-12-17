7f2fc2edcd72e30e9171362aed46b92c1b488413
Ashok Gelal <ashok@metageek.net>
2018-12-17 16:07:59 -0700

    Parse json

--- lib/servy/bear.ex
+++ lib/servy/bear.ex
@@ -1,5 +1,5 @@
 defmodule Servy.Bear do
-  defstruct id: nil, name: "", type: "", hibernating: true
+  defstruct id: nil, name: "", type: "", hibernating: false
 
   def is_grizzly(bear) do
     bear.type == "Grizzly"
@@ -8,4 +8,5 @@ defmodule Servy.Bear do
   def order_asc_by_name(b1, b2) do
     b1.name <= b2.name
   end
+
 end
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,20 +1,24 @@
 defmodule Servy.Parser do
+
   alias Servy.Conv
 
   def parse(request) do
-    [top, params_string] = String.split(request, "\n\n")
-    [request_line | header_lines] = String.split(top, "\n")
+    [top, params_string] = String.split(request, "\r\n\r\n")
+
+    [request_line | header_lines] = String.split(top, "\r\n")
+
     [method, path, _] = String.split(request_line, " ")
 
     headers = parse_headers(header_lines, %{})
+
     params = parse_params(headers["Content-Type"], params_string)
 
-    %Conv{
-      method: method,
-      path: path,
-      params: params,
-      headers: headers
-    }
+    %Conv{ 
+       method: method, 
+       path: path,
+       params: params,
+       headers: headers
+     }
   end
 
   def parse_headers([head | tail], headers) do
@@ -25,11 +29,21 @@ defmodule Servy.Parser do
 
   def parse_headers([], headers), do: headers
 
+  @doc """
+  Parses the given param string of the form `key1=value1&key2=value2` 
+  into a map with corresponding keys and values.
+
+  ## Examples
+      iex> params_string = "name=Baloo&type=Brown"
+      iex> Servy.Parser.parse_params("application/x-www-form-urlencoded", params_string)
+      %{"name" => "Baloo", "type" => "Brown"}
+      iex> Servy.Parser.parse_params("multipart/form-data", params_string)
+      %{}
+  """
   def parse_params("application/x-www-form-urlencoded", params_string) do
-    params_string
-    |> String.trim
-    |> URI.decode_query
+    params_string |> String.trim |> URI.decode_query
   end
 
   def parse_params(_, _), do: %{}
+
 end
